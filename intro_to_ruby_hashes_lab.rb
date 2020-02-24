c36a06902de8258c4aa07bb49269cca65fa8acf0
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-24 15:36:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,15 +16,9 @@ end
 def monopoly_with_third_tier
   monopoly = {
     :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_piece_owned => 50,
-        :three_piece_owned => 100,
-        :four_piece_owned => 200
-      },
-      :names = {}
-    },
+      :pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, :two_piece_owned => 50, :three_piece_owned => 100, :four_piece_owned => 200}, :names = {}
+      
+    }
   }
 end
 
