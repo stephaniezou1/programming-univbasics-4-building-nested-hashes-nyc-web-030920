700cf816325927192606b3057d210c5eaee44865
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-24 15:34:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
         :two_piece_owned => 50,
         :three_piece_owned => 100,
         :four_piece_owned => 200
-      }
+      },
       :names = {}
     }
   }
